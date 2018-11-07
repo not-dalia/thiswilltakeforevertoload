9b77388c42c8efe3af83ee8855531751a5bd637c
Dalya <LocalAdmin@16DCompL069.campus.ncl.ac.uk>
2018-11-07 15:17:50 +0000

    added main page and toomanyredirects

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('taking forever to load');
 });
+
+
+router.get('/toomanyredirects', function(req, res, next) {
+  setTimeout(function(){res.redirect('/toomanyredirects');}, 3000);
+});
+
+router.get('/help', function(req, res, next) {
+  res.render('index', {title: 'Help - This will take forever to load'});
+});
+
+router.get('/:anything', function(req, res, next) {
+  res.redirect('/help');
+});
+
+
 
 module.exports = router;
